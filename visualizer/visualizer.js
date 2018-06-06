ec8ba1f28bddd5457fa1ed2fc762e6c29cfbfd61
Hank Wikle <henry.wikle@gmail.com>
2018-06-06 10:11:16 -0600

    implemented wave equation

--- visualizer/visualizer.js
+++ visualizer/visualizer.js
@@ -26,14 +26,18 @@ const GAIN = 1;
 export class Visualizer extends Model {
   setup(buffer) {
     this.ticks = 0;
-    this.patches.own("energy");
+    this.patches.own("energy", "energyVelocity");
     this.turtles.own("waveNum", "frequency", "power", "speed");
     this.numWaves = buffer.length;
     this.buffer = buffer;
     this.cmap = ColorMap.Jet;
-    this.diffusionRate = 1;
-    this.dissipationRate = 0.005;
+    this.diffusionRate = 0;
+    this.dissipationRate = 0;
     this.speed = 0.3;
+    this.friction = 0.3;
+    this.surfaceTension = 30;
+    this.k = 1 - (0.01 * this.surfaceTension);
+    this.energyLimit = 10000;
 
     this.turtles.create(this.buffer.length, t => {
         t.setxy(Math.random() * (this.world.maxX - this.world.minX) + this.world.minX,
@@ -45,6 +49,7 @@ export class Visualizer extends Model {
 
     this.patches.ask(p => {
         p.energy = 0;
+        p.energyVelocity = 0;
       });
 
     this.turtles.setDefault('atEdge', 'bounce');
@@ -53,7 +58,9 @@ export class Visualizer extends Model {
   step() {
     this.turtles.ask(t => {
       //t.power = (this.buffer[t.waveNum] * Math.log(t.frequency)/Math.log(16*SAMPLE_RATE/FFT_SIZE));
-      t.power = this.buffer[t.waveNum] * t.frequency * 10/(16*SAMPLE_RATE/FFT_SIZE);
+      t.power = this.buffer[t.waveNum] * t.frequency /(16*SAMPLE_RATE/FFT_SIZE);
+      t.power /= 2550;
+      //t.power = 0;
       t.patch.energy += t.power;
       t.forward(this.speed);
     });
@@ -61,10 +68,23 @@ export class Visualizer extends Model {
     this.patches.diffuse("energy", this.diffusionRate);
     this.patches.ask(p => {
         p.energy *= 1 - this.dissipationRate;
+        
+        let neighborEnergy = 0;
+        for (let i=0; i<p.neighbors.length; i++)
+            neighborEnergy += p.neighbors[i].energy;
+
+        p.energyVelocity = this.friction * (p.energyVelocity + this.k * (neighborEnergy - p.energy * p.neighbors.length));
+        p.energy += p.energyVelocity;
+        
+        if (p.energy > this.energyLimit)
+            p.energy = this.energyLimit;
+        else if (p.energy < 0)
+            p.energy = 0;
+
+        //console.log(p.energy);
     });
     
-    let maxEnergy = 500;
-    this.patches.scaleColors(ColorMap.Jet, "energy", 0, maxEnergy);
+    this.patches.scaleColors(ColorMap.Jet, "energy", 0, this.energyLimit);
     this.ticks++;
   }
 }
