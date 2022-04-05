b0a5783dc56def013cb6299b41d8df9721c22e3e
cody <cody@simtable.com>
2022-04-04 21:51:24 -0600

    added terrain test

--- levelOfDetail/meshUtils.js
+++ levelOfDetail/meshUtils.js
@@ -1,3 +1,7 @@
+import * as Martini from 'https://unpkg.com/@mapbox/martini@0.2.0/martini.min.js'
+
+console.log('martini', Martini)
+
 function meshFromElevationTile(tile) {
   if (tile && tile.width > 0 && tile.height > 0) {
     //
