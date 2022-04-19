af579f42cebe98668da9b62ccd896dcb00fa365f
cody <cody@simtable.com>
2022-04-18 18:20:55 -0600

    better

--- levelOfDetail/utils.js
+++ levelOfDetail/utils.js
@@ -53,32 +53,10 @@ export function splitTileCoordinates(x, y, z) {
 }
 
 /**
- *
- * Pad a dataset on all sides, by a certain number of pixels, with the value of the closest point.
- *
- * @param {DataSet} ds
- * @param {Number} pixels
- * @returns {DataSet}
+ * Pad the bottom and right of the Dataset with 1 pixel, with a value that maintins the slope at the edges. 
+ * @param {DataSet} ds 
+ * @returns 
  */
-export function padDataSet(ds, pixels = 1) {
-    const newWidth = ds.width + 2 * pixels
-    const newHeight = ds.height + 2 * pixels
-    const newData = new DataSet(
-        newWidth,
-        newHeight,
-        new ds.data.constructor(newWidth * newHeight)
-    )
-    for (let j = 0; j < newHeight; j++) {
-        for (let i = 0; i < newWidth; i++) {
-            const x = Math.min(ds.width - 1, Math.max(0, i - pixels))
-            const y = Math.min(ds.height - 1, Math.max(0, j - pixels))
-            const v = ds.sample(x, y)
-            newData.setXY(i, j, v)
-        }
-    }
-    return newData
-}
-
 export function padDataBottomAndRight(ds) {
     const newWidth = ds.width + 1
     const newHeight = ds.height + 1
@@ -92,6 +70,7 @@ export function padDataBottomAndRight(ds) {
             const x = Math.min(ds.width-1, Math.max(0, i ))
             const y = Math.min(ds.height-1, Math.max(0, j ))
             let v = ds.sample(x, y, false)
+            // maintain the slope along the edges. 
             if (i >= ds.width) {
                 const u2 = ds.sample(ds.width - 2, y)
                 const u1 = ds.sample(ds.width - 1, y)
@@ -107,41 +86,71 @@ export function padDataBottomAndRight(ds) {
     return newData
 }
 
+
+
+/**
+ *
+ * Pad a dataset on all sides, by a certain number of pixels, with the value of the closest point.
+ *
+ * @param {DataSet} ds
+ * @param {Number} pixels
+ * @returns {DataSet}
+ */
+// export function padDataSet(ds, pixels = 1) {
+//     const newWidth = ds.width + 2 * pixels
+//     const newHeight = ds.height + 2 * pixels
+//     const newData = new DataSet(
+//         newWidth,
+//         newHeight,
+//         new ds.data.constructor(newWidth * newHeight)
+//     )
+//     for (let j = 0; j < newHeight; j++) {
+//         for (let i = 0; i < newWidth; i++) {
+//             const x = Math.min(ds.width - 1, Math.max(0, i - pixels))
+//             const y = Math.min(ds.height - 1, Math.max(0, j - pixels))
+//             const v = ds.sample(x, y)
+//             newData.setXY(i, j, v)
+//         }
+//     }
+//     return newData
+// }
+
+
 /**
  * Pad the dataset with 1 pixel while maintaining the slope at the borders.
  *
  * @param {DataSet} ds0
  * @returns
  */
-export function padDataSetMaintainSlope(ds0) {
-    const ds = padDataSet(ds0, 1)
-    //top
-    for (let x = 0; x < ds.width; x++) {
-        const v1 = ds.getXY(x, 1)
-        const gradient = ds.getXY(x, 2) - ds.getXY(x, 1)
-        const val = v1 - gradient
-        ds.setXY(x, 0, val)
-    }
-    // bottom
-    for (let x = 0; x < ds.width; x++) {
-        const v1 = ds.getXY(x, ds.height - 2)
-        const gradient = ds.getXY(x, ds.height - 2) - ds.getXY(x, ds.height - 3)
-        const val = v1 + gradient
-        ds.setXY(x, ds.height - 1, val)
-    }
-    // left
-    for (let y = 0; y < ds.height; y++) {
-        const v1 = ds.getXY(1, y)
-        const gradient = ds.getXY(2, y) - ds.getXY(1, y)
-        const val = v1 - gradient
-        ds.setXY(0, y, val)
-    }
-    // right
-    for (let y = 0; y < ds.height; y++) {
-        const v1 = ds.getXY(ds.width - 2, y)
-        const gradient = ds.getXY(ds.width - 2, y) - ds.getXY(ds.width - 3, y)
-        const val = v1 + gradient
-        ds.setXY(ds.width - 1, y, val)
-    }
-    return ds
-}
+// export function padDataSetMaintainSlope(ds0) {
+//     const ds = padDataSet(ds0, 1)
+//     //top
+//     for (let x = 0; x < ds.width; x++) {
+//         const v1 = ds.getXY(x, 1)
+//         const gradient = ds.getXY(x, 2) - ds.getXY(x, 1)
+//         const val = v1 - gradient
+//         ds.setXY(x, 0, val)
+//     }
+//     // bottom
+//     for (let x = 0; x < ds.width; x++) {
+//         const v1 = ds.getXY(x, ds.height - 2)
+//         const gradient = ds.getXY(x, ds.height - 2) - ds.getXY(x, ds.height - 3)
+//         const val = v1 + gradient
+//         ds.setXY(x, ds.height - 1, val)
+//     }
+//     // left
+//     for (let y = 0; y < ds.height; y++) {
+//         const v1 = ds.getXY(1, y)
+//         const gradient = ds.getXY(2, y) - ds.getXY(1, y)
+//         const val = v1 - gradient
+//         ds.setXY(0, y, val)
+//     }
+//     // right
+//     for (let y = 0; y < ds.height; y++) {
+//         const v1 = ds.getXY(ds.width - 2, y)
+//         const gradient = ds.getXY(ds.width - 2, y) - ds.getXY(ds.width - 3, y)
+//         const val = v1 + gradient
+//         ds.setXY(ds.width - 1, y, val)
+//     }
+//     return ds
+// }
